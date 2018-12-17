f65813f0624a19ac92bdcd2edb7efd0ef6b84805
hzwirezone <info@hzwirezone.cn>
2018-12-17 17:31:59 +0800

    home-commit

--- pages/home/index.js
+++ pages/home/index.js
@@ -11,7 +11,12 @@ Page({
     logos:[],
     pageRow:[],
     quicks: [],
-    swipers:[]
+    swipers:[],
+    indicatorDots:true,
+    vertical:false,
+    autoplay:true,
+    interval:3000,
+    duration:500
   },
 
   /**
